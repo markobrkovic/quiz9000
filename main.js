8e20e2f62c8d316bfcf9dd86dae75ecc6c25eb6c
markobrkovic <querry999@gmail.com>
2021-10-11 17:19:59 +0200

    Add a conditional to main.js

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
+const correcrAnswer = false;
+
 const myQuestion = document.querySelector('.question');
-myQuestion.textContent = 'Some random question generator';
+myQuestion.textContent = 'Mars is a moon';
+
+
+const yesButton = document.querySelector('.yes');
+const noButton = document.querySelector('.no');
+
+yesButton.onclick = function () {
+    if(correcrAnswer === true) {
+        alert("True");
+    } else {
+        alert("False");
+    }
+};
+
+noButton.onclick = function () {
+    if(correcrAnswer === false) {
+        alert("True");
+    } else {
+        alert("False");
+    }
+};
