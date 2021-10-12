e9abed72aab02ee889d193300048e3e668485a58
markobrkovic <querry999@gmail.com>
2021-10-12 14:38:48 +0200

    Add lots of stuff

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
-const correcrAnswer = false;
+const correctAnswer = false;
 
-const myQuestion = document.querySelector('.question');
-myQuestion.textContent = 'Mars is a moon';
+const myQuestion = document.querySelector(".question");
+myQuestion.textContent = "Mars is a moon";
 
-
-const yesButton = document.querySelector('.yes');
-const noButton = document.querySelector('.no');
+const yesButton = document.querySelector(".yes");
+const noButton = document.querySelector(".no");
 
 yesButton.onclick = function () {
-    if(correcrAnswer === true) {
-        alert("True");
-    } else {
-        alert("False");
-    }
+  if (correctAnswer === true) {
+    showAnswerIsCorrect();
+  } else {
+    showAnswerIsWrong();
+  }
+  removePreviousResult();
 };
 
 noButton.onclick = function () {
-    if(correcrAnswer === false) {
-        alert("True");
-    } else {
-        alert("False");
-    }
+  if (correctAnswer === false) {
+    showAnswerIsCorrect();
+  } else {
+    showAnswerIsWrong();
+  }
+  removePreviousResult();
 };
+
+function showAnswerIsCorrect() {
+  const newParagraph = document.createElement("p");
+  newParagraph.classList.add("correct");
+  newParagraph.setAttribute("id", "message");
+  newParagraph.textContent = "Correct";
+  document.body.appendChild(newParagraph);
+  disableButtons();
+}
+
+function showAnswerIsWrong() {
+  const newParagraph = document.createElement("p");
+  newParagraph.classList.add("wrong");
+  newParagraph.setAttribute("id", "message");
+  newParagraph.textContent = "Wrong";
+  document.body.appendChild(newParagraph);
+  disableButtons();
+}
+
+function disableButtons() {
+  yesButton.disabled = true;
+  noButton.disabled = true;
+}
