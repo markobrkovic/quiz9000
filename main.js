3a56fc76a5f1c684adf469a46e3678bdff2ebd04
markobrkovic <querry999@gmail.com>
2021-10-13 16:02:30 +0200

    Add Object Array

--- main.js
+++ main.js
@@ -1,17 +1,114 @@
-const questionList = [
-  "Is GME gonna moon?",
-  "Is 2+2 = 4?",
-  "Are you an ape?",
-  "Is 2+2 = 3?",
+// const questionList = [
+//   "Is this a quiz?",
+//   "Is the name of the quiz 'Quiz 9001'?",
+//   "Is the backgound blue?",
+//   "Is the font pink?",
+//   "Are there round corners?",
+//   "Do you still enjoy this quiz?",
+// ];
+
+// let questionIndex = 0;
+// const correctAnswer = [true, false, true, false, true, false];
+
+// //Question counter
+// function updateProgress() {
+//   const progressElement = document.querySelector(".progress");
+//   progressElement.textContent = `Question ${questionIndex + 1}/${
+//     questionList.length
+//   }`;
+//   // same as: "Question " + (questionIndex + 1) + "/" + questionList.length;
+// }
+
+// // Question
+// const question = document.querySelector(".question");
+// question.textContent = questionList[0];
+// updateProgress();
+
+// function nextQuestion() {
+//   questionIndex += 1;
+//   if (questionIndex >= correctAnswer.length) {
+//     disableButtons(true);
+//   } else {
+//     disableButtons(false);
+//     const nextButton = document.createElement("p");
+//     nextButton.textContent = "Next Question";
+//     nextButton.className = "nextButton";
+//     Print.appendChild(nextButton);
+//     nextButton.onclick = function () {
+//       question.textContent = questionList[questionIndex];
+//       // questionIndex += 1;
+//       Print.removeChild(document.querySelector(".correct"));
+//     };
+//     updateProgress();
+//   }
+// }
+
+// // Yes-Button
+// const yesButton = document.querySelector(".yes");
+// yesButton.onclick = function () {
+//   if (correctAnswer[questionIndex] === true) {
+//     showAnswerIsCorrect();
+//   } else {
+//     showAnswerIsIncorrect;
+//   }
+// };
+
+// // No-Button
+// const noButton = document.querySelector(".no");
+// noButton.onclick = function () {
+//   if (correctAnswer[questionIndex] === false) {
+//     showAnswerIsCorrect();
+//   } else {
+//     showAnswerIsIncorrect();
+//   }
+// };
+
+// function showAnswerIsCorrect() {
+//   // Variable resultElement anlegen, die ein p erstellt
+
+//   const resultElement = document.createElement("p");
+//   // p hat den Inhalt
+//   resultElement.textContent = "That’s right! 🙃";
+//   // p hat die Klasse
+//   resultElement.className = "correct";
+//   // resultElement dem body hinzufügen (append am Ende. prepend am Anfang.)
+//   // document.body.append(resultElement);
+//   Print.appendChild(resultElement);
+//   disableButtons(true);
+//   nextQuestion();
+// }
+
+// function showAnswerIsIncorrect() {
+//   disableButtons(true);
+//   const resultElement = document.createElement("p");
+//   resultElement.textContent = "That’s wrong 🙁";
+//   resultElement.className = "incorrect";
+//   Print.appendChild(resultElement);
+//   disableButtons(true);
+//   nextQuestion();
+// }
+
+// function disableButtons(currentState) {
+//   yesButton.disabled = currentState;
+//   noButton.disabled = currentState;
+// }
+
+const questionsAndAnswers = [
+  { question: "LAMBO?", answer: true },
+  { question: "Are you an ape?", answer: true },
+  { question: "Is 1 Mill new floor?", answer: true },
+  { question: "Exit strategy?", answer: false },
 ];
+
 let questionIndex = 0;
-const correctAnswer = [true, true, true, false];
 
 const myQuestion = document.querySelector(".question");
-myQuestion.textContent = questionList[0];
+myQuestion.textContent = questionsAndAnswers[questionIndex].question;
 
 const questionNumber = document.querySelector(".questionNumber");
-questionNumber.textContent = `#${questionIndex + 1}/${questionList.length}`;
+questionNumber.textContent = `#${questionIndex + 1}/${
+  questionsAndAnswers.length
+}`;
 
 const oldDiv = document.querySelector("div");
 const yesButton = document.querySelector(".yes");
@@ -19,7 +116,7 @@ const noButton = document.querySelector(".no");
 
 yesButton.onclick = function () {
   //   removeParagraph();
-  if (correctAnswer[questionIndex] === true) {
+  if (questionsAndAnswers[questionIndex].answer === true) {
     showAnswerIsCorrect();
   } else {
     showAnswerIsWrong();
@@ -28,7 +125,7 @@ yesButton.onclick = function () {
 
 noButton.onclick = function () {
   //   removeParagraph();
-  if (correctAnswer[questionIndex] === false) {
+  if (questionsAndAnswers[questionIndex].answer === false) {
     showAnswerIsCorrect();
   } else {
     showAnswerIsWrong();
@@ -36,23 +133,25 @@ noButton.onclick = function () {
 };
 
 function showAnswerIsCorrect() {
-  const newParagraph = document.createElement("p");
-  newParagraph.classList.add("correct");
-  newParagraph.setAttribute("id", "message");
-  newParagraph.textContent = "Correct";
+  const newParagraph = createMyElement("p", {
+    className: "correct",
+    textContent: "Correct",
+    id: "message",
+  });
   oldDiv.appendChild(newParagraph);
   disableButtons(true);
-  setTimeout(() => setNewQuestion("Is GME gonna moon?"), 3000);
+  setTimeout(setNewQuestion, 1000);
 }
 
 function showAnswerIsWrong() {
-  const newParagraph = document.createElement("p");
-  newParagraph.classList.add("wrong");
-  newParagraph.setAttribute("id", "message");
-  newParagraph.textContent = "Wrong";
+  const newParagraph = createMyElement("p", {
+    className: "wrong",
+    textContent: "Incorrect",
+    id: "message",
+  });
   oldDiv.appendChild(newParagraph);
   disableButtons(true);
-  setTimeout(() => setNewQuestion("Is GME gonna moon?"), 3000);
+  setTimeout(setNewQuestion, 1000);
 }
 
 function disableButtons(currentState) {
@@ -64,14 +163,26 @@ function setNewQuestion() {
   disableButtons(false);
   oldDiv.removeChild(document.querySelector("#message"));
   questionIndex += 1;
-  if (questionIndex >= questionList.length) {
-    myQuestion.textContent = "Congratulations, you finished the test!";
+  if (questionIndex >= questionsAndAnswers.length) {
+    myQuestion.textContent = "Congratulations, you finished the quiz!";
     disableButtons(true);
+    document.querySelector(".yes").classList.add("dissapear");
+    document.querySelector(".no").classList.add("dissapear");
+    document.querySelector(".questionNumber").classList.add("dissapear");
   } else {
-    myQuestion.textContent = questionList[questionIndex];
-    questionNumber.textContent = `#${questionIndex + 1}/${questionList.length}`;
+    myQuestion.textContent = questionsAndAnswers[questionIndex].question;
+    questionNumber.textContent = `#${questionIndex + 1}/${
+      questionsAndAnswers.length
+    }`;
   }
-  //   nextButton.onclick()
+}
+
+function createMyElement(tagName, properties) {
+  const newElement = document.createElement(tagName);
+  newElement.className = properties.className;
+  newElement.textContent = properties.textContent;
+  newElement.id = properties.id;
+  return newElement;
 }
 
 // WIthout disabled buttons
